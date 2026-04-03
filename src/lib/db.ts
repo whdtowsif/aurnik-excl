8e1c35fbe02b7dd71698760ab91c70257fd0dfb9
Z User <z@container>
2026-04-03 10:12:37 +0000

    Configure Turso database for serverless deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+})
+
+const adapter = new PrismaLibSQL(libsql)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -7,6 +16,7 @@ const globalForPrisma = globalThis as unknown as {
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ['query'],
   })
 
